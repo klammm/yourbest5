adf8e0b501c39b062de558f7404acfaf3e84eca5
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-03-20 19:59:27 -0700

    add user and team table with relationship

---
+++ migrations/20170320194429_teams.js
@@ -0,0 +1,36 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('teams', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('teams')
+};
+
+
+
+
+
+
+
+
+
+
+// exports.up = function(knex, Promise) {
+//   return knex.schema.createTable('users', (table) => {
+//     table.increments('id').primary();
+//     table.string('first_name').defaultTo('');
+//     table.string('last_name').defaultTo('');
+//     table.string('email').unique().notNullable()
+//     table.specificType('hashed_password', 'char(60)').notNullable();
+//     table.integer('team_id').notNullable().references('id').inTable('teams').onDelete('CASCADE');
+//     table.timestamps(true, true);
+//     })
+//   };
+//
+// exports.down = function(knex, Promise) {
+//   return knex.schema.dropTable('users');
+// };
--- migrations/20170320195734_users.js
+++ migrations/20170320195734_users.js
@@ -2,11 +2,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
-    table.string('first_name').notNullable().defaultTo('');
-    table.string('last_name').notNullable().defaultTo('');
-    table.string('email').unique().notNullable();
+    table.string('first_name').defaultTo('');
+    table.string('last_name').defaultTo('');
+    table.string('email').unique().notNullable()
     table.specificType('hashed_password', 'char(60)').notNullable();
-    table.timestamps(true,true);
+    table.integer('team_id').notNullable().references('id').inTable('teams').onDelete('CASCADE');
+    table.timestamps(true, true);
     })
   };
 
