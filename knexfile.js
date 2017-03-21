1cee88a5685f703d4d601905187d1836aeb8af7e
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-03-20 19:17:17 -0700

    add user migration table

--- knexfile.js
+++ knexfile.js
@@ -6,37 +6,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgress://localhost/nba_dev'
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-  //
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+  test: {
+    client: 'pg',
+    connection: 'postgress://localhost/nba_test'
+  },
 };
