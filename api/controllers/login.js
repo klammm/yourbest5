2a525ec6ce37d4afb3a726280fbbd0cce99d0636
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-03-30 20:28:03 -0700

    Add user login route

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -1,12 +1,8 @@
 'use strict';
-var util = require('util');
+let util = require('util');
 const knex = require('../../knex');
 const bcrypt = require('bcrypt-as-promised');
 const jwt = require('jsonwebtoken');
-const express = require('express');
-// const app = express();
-// const cookieParser = require('cookie-parser');
-// app.use(cookieParser())
 const dotenv = require('dotenv');
 dotenv.load()
 
@@ -16,7 +12,6 @@ function userLogin(req, res) {
     .where('email', req.body.email)
     .first()
     .then((user) => {
-      console.log(user);
       if (!user) {
         res.status(400).send('Invalid email');
       }
@@ -27,18 +22,17 @@ function userLogin(req, res) {
           if (!auth) {
             res.status(400).send('Invalid password');
           }
-          // console.log(auth);
-          const token = jwt.sign({
+          const token = jwt.sign({id: authUser.id},
+            process.env.JWT_KEY);
+
+            let userInfo = {
               id: authUser.id,
               email: authUser.email,
               first_name: authUser.first_name,
               last_name: authUser.last_name,
-            },
-            process.env.JWT_KEY);
-
-          return res.status(200).json({
-            token
-          })
+              token: token
+              }
+          return res.status(200).send(userInfo)
         })
         .catch((err) => {
           return res.status(400).json('bad email or password')
