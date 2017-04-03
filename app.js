41430b86079695039029398f95b35c627a303ba3
Kevin Lam <kevinlam.ucsb@gmail.com>
2017-04-03 13:22:56 -0700

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var SwaggerExpress = require('swagger-express-mw');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+app.use(cors());
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -11,7 +12,12 @@ if (process.env.NODE_ENV !== 'production') {
 
 const path = require('path');
 app.use(express.static(path.join('public')));
-app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var config = {
   appRoot: __dirname // required config
